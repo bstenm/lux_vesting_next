49564f4f55f5bab1d05b8c76378bebe7ba1f8b6c
Bertrand Steinmetz <bstenm@hotmail.com>
2023-06-29 12:24:19 +0200

    Sending email through route handler working

--- src/features/userMenu/UserMenu.tsx
+++ src/features/userMenu/UserMenu.tsx
@@ -1,7 +1,5 @@
-import { path } from 'config/path';
 import { useWeb3 } from 'features/authButton/useWeb3';
 import { MenuPopover, MenuPopoverItem } from 'components/MenuPopover';
-import { SITE_URL } from 'config/constants';
 
 type Props = {
     anchorEl: HTMLButtonElement | null;
@@ -11,32 +9,12 @@ type Props = {
 export function UserMenu({ anchorEl, handleClose }: Props): JSX.Element {
     const { logout } = useWeb3();
 
-    const sendMailUrl = `${SITE_URL}/${path.sendEmail}`;
-
-    const sendEmail = async (): Promise<void> => {
-        try {
-            await fetch(sendMailUrl, {
-                method: 'POST',
-                body: JSON.stringify({
-                    to: 'bstenm@hotmail.com',
-                    subject: 'Lux Vesting New Listing'
-                }),
-                headers: { 'Content-Type': 'application/json' },
-                mode: 'no-cors' // disable cors
-            });
-            alert('Message sent');
-        } catch (error) {
-            console.log(error);
-            alert('Something went wrong. Please try again.');
-        }
-    };
-
     return (
         <MenuPopover
             open={Boolean(anchorEl)}
             onClose={handleClose}
             anchorEl={anchorEl}>
-            <MenuPopoverItem onClick={sendEmail}>My activity</MenuPopoverItem>
+            <MenuPopoverItem>My activity</MenuPopoverItem>
             <MenuPopoverItem onClick={logout}>Logout</MenuPopoverItem>
         </MenuPopover>
     );
